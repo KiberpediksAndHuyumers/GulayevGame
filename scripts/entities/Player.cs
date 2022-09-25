141a67602064df5bff93550b4b21b86c5666de4e
Gastyach <kiberpediksandhuyumers@gmail.com>
2022-09-25 15:02:01 +0200

    Создание комнаты
    Добавила на сцене test пол и стены, образуя небольшую замкнутую комнату.

--- scripts/entities/Player.cs
+++ scripts/entities/Player.cs
@@ -3,46 +3,46 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    [Export] Vector2 upDirection = Vector2.Up;
+	[Export] Vector2 upDirection = Vector2.Up;
 
-    [Export] float speed = 600;
+	[Export] float speed = 600;
 
-    [Export] float jumpStrength = 1500;
-    [Export] int maxJumps = 2;
-    [Export] float doubleJumpStrength = 1200;
-    [Export] float gravity = 4500;
+	[Export] float jumpStrength = 1500;
+	[Export] int maxJumps = 2;
+	[Export] float doubleJumpStrength = 1200;
+	[Export] float gravity = 4500;
 
-    int jumpsMade;
-    Vector2 velocity = Vector2.Zero;
+	int jumpsMade;
+	Vector2 velocity = Vector2.Zero;
 
-    Sprite sprite;
+	Sprite sprite;
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 
-        sprite = GetNode<Sprite>("CollisionShape2D/Sprite");
-    }
+		sprite = GetNode<Sprite>("CollisionShape2D/Sprite");
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        base._PhysicsProcess(delta);
+	public override void _PhysicsProcess(float delta)
+	{
+		base._PhysicsProcess(delta);
 
-        float horizontalDirection = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        float verticalDirection = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+		float horizontalDirection = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		float verticalDirection = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
-        velocity.x = horizontalDirection * speed;
-        velocity.y = verticalDirection * speed / 1.5f;
+		velocity.x = horizontalDirection * speed;
+		velocity.y = verticalDirection * speed / 1.5f;
 
-        if (velocity.x < 0)
-        {
-            sprite.FlipH = true;
-        }
-        else if (velocity.x > 0)
-        {
-            sprite.FlipH = false;
-        }
+		if (velocity.x < 0)
+		{
+			sprite.FlipH = true;
+		}
+		else if (velocity.x > 0)
+		{
+			sprite.FlipH = false;
+		}
 
-        velocity = MoveAndSlide(velocity, upDirection);
-    }
+		velocity = MoveAndSlide(velocity, upDirection);
+	}
 }
